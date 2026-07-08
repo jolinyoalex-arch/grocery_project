677a8950726e323f2c0a495b0fb94e42fabcdc14
jolinyo <jolinyoalex@gmail.com>
2026-07-08 23:52:00 +0000

    Kuweka admin wa john mtandaoni

--- grocery_project/orderingapp/reset_admin.py
+++ grocery_project/orderingapp/reset_admin.py
@@ -1,17 +1,8 @@
-import os
-import django
-
-# Weka mazingira ya Django (Settings)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_project.settings') # Kama jina la folda la settings ni tofauti, badilisha hapa
-django.setup()
-
-from django.contrib.auth import get_user_model
-
 def reset():
     User = get_user_model()
-    username = 'jolinyo'
+    username = 'john'
     password = 'jolinyo001' # Hii ndio itakuwa password yako mpya
-    email = 'jolinyoalex@gmail.com'
+    email = 'john@gmail.com'
 
     user = User.objects.filter(username=username).first()
     if user:
@@ -21,6 +12,3 @@ def reset():
     else:
         User.objects.create_superuser(username=username, email=email, password=password)
         print(f"✅ SUCCESS: Admin mpya '{username}' ametengenezwa kikamilifu kule Render!")
-
-if __name__ == '__main__':
-    reset()
